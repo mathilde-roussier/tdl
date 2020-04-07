0da5c6be536656390b7a1a948efc29e932eee27c
samuel-joly <samuel-joly@laplateforme.io>
2020-04-07 15:53:23 +0200

    End of basic BDD

--- class/bdd.php
+++ class/bdd.php
@@ -53,6 +53,21 @@ class bdd
 		}
 	}
 
+	public function add_tableau($id_createur, $titre)
+	{
+		$query = "SELECT * FROM taches WHERE id_createur=".$id_createur." AND nom='".$titre."'";
+		$test = $this->connexion->query($query)->fetch();
+		if(empty($test))
+		{
+			if($this->connexion->query("INSERT INTO `tablaeux`(`id`, `id_createur`, `nom`)
+						   VALUES  (NULL, '".$id_createur."', '".$titre."')" ) )
+			{
+				$id = $this->connexion->query($query)->fetch()["id"];
+				echo json_encode(["titre"=>$titre, "id_createur"=>$id_createur, "id"=>$id]);
+			}
+		}
+	}
+
 	public function del($type, $id)
 	{
 		if($type == 1)
@@ -63,14 +78,44 @@ class bdd
 		{
 			$this->connexion->query("DELETE FROM taches WHERE id=".$id);
 		}
+		else if($type == 3)
+		{
+			$this->connexion->query("DELETE FROM tableaux WHERE id=".$id);
+		}
 	}
 
 	public function update($id, $table, $column, $value)
 	{
 		$this->connexion->query("UPDATE ".$table." SET ".$column."='".$value."' WHERE id=".$id);
 	}
-}
-$bd = new bdd();
 
-$bd->add_list(1,2,"chocotask");
+	public function get_tableaux($id_createur)
+	{
+		$query = "SELECT * FROM tableaux WHERE id_createur=".$id_createur;
+		$res = $this->connexion->query($query)->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($res);
+		return $res;
+	}
+
+	public function get_listes($id_createur, $id_tableau)
+	{
+		$query = "SELECT * FROM listes WHERE id_createur=".$id_createur." AND id_tableau=".$id_tableau;
+		$res = $this->connexion->query($query)->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($res);
+		return $res;
+	}
+
+	public function get_taches($id_createur, $id_liste, $id_tableau)
+	{
+		$query = "SELECT *, taches.nom AS tache_nom, taches.id AS tache_id FROM taches 
+		INNER JOIN listes ON taches.id_liste = listes.id 
+		INNER JOIN tableaux ON listes.id_tableau = tableaux.id 
+		WHERE taches.id_createur=".$id_createur." AND taches.id_liste=".$id_liste." AND  listes.id_tableau=".$id_tableau;
+		$res = $this->connexion->query($query)->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($res);
+		var_dump($res);
+		return $res;
+	}
+}
+
 ?>
